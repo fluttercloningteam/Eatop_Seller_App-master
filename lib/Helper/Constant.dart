8c313ec538cd4b1885625466f51c69709279b9ff
Alphawizz Flutter Team <alphawizzflutterteam@gmail.com>
2024-05-14 12:34:30 +0530

    Initial commit by Anjali

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,22 @@
+final String appName = 'Eatop';
+
+final String packageName = 'com.eatop.seller';
+
+final String iosPackage = 'com.eatop.seller';
+
+// https://alphawizztest.tk/Food_on_the_way/app/v1/api/
+// final String baseUrl = 'https://foodontheways.com/seller/app/v1/api/';
+//final String baseUrl = 'https://foodontheways.com/New_food/seller/app/v1/api/';
+// /*final String baseUrl = 'https://eatoz.in/seller/app/v1/api/';*/
+final String baseUrl = 'https://developmentalphawizz.com/zipcart/seller/app/v1/api/';
+// final String baseUrl = 'https://developmentalphawizz.com/eatoz/seller/app/v1/api/';
+final String jwtKey = "f78d95e500c7ef84ee99d69d9651ff7a229979a2";
+
+//timeout limit
+final int timeOut = 50;
+const int perPage = 10;
+
+const String BANK_DETAIL = 'Bank Details:\n' +
+    'Account No :123XXXXX\n' +
+    'IFSC Code: 123XXX \n' +
+    'Name: Abc xyz';
